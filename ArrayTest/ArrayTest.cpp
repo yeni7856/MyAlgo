73fb5c067881a3d7d25587aaf7bba15436181d74
yeni7856 <86287877+yeni7856@users.noreply.github.com>
2024-11-22 17:59:43 +0900

    1. vector 예제 2. LinkedList 예제

--- ArrayTest/ArrayTest.cpp
+++ ArrayTest/ArrayTest.cpp
@@ -3,26 +3,135 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
 
 using namespace std;
 
-//동적 메모리 할당예제
+//2행 3열 원소의 출력하라
+//c++ vector 2차원 배열 활용
 int main()
 {
-	//동적 메모리 할당
-	int* ptr = new int[3] {};
-	ptr[0] = 10;
-	ptr[1] = 20;
+	//2차원 배열 선언 및 초기화
+	//배열에 int vector을 가지고잇는 배열을 가지고있음
+	vector<vector<int>> mat1(2, vector<int>(3, 0));			//(3,0) 방3개짜리
+	vector<vector<int>> mat2{
+		{1,2,3},
+		{4,5,6}
+	};
 
-	for (int i = 0; i < 3; i++)
+	//출력
+	for (int r = 0; r < mat2.size(); r++)
 	{
-		cout << ptr[i] << endl;
+		for (int c = 0; c < mat2[r].size(); c++)
+		{
+			cout << mat2[r][c] << " ";
+		}
+		cout << endl;
 	}
-	//동적 메모리 해제
-	delete[] ptr;
-	ptr = nullptr;
 }
+//output
+// 1 2 3
+// 4 5 6
 
+//2행 3열 원소의 합을 구하라
+//c언어 2차원 배열
+//int main()
+//{
+//	int mat[2][3] = {
+//		{1, 2, 3},
+//		{4,5,6}
+//	};
+//	int sum = 0;
+//	for (int r = 0; r < 2; r++)
+//	{
+//		for (int c = 0; c < 3; c++)
+//		{
+//			sum += mat[r][c];
+//		}
+//	}
+//	cout << "Sum : " << sum << endl;
+//}
+
+//가변형 배열 
+//int main()
+//{
+//	vector<int> v1;
+//	v1.push_back(10);
+//	v1.push_back(20);
+//	v1.push_back(30);
+//
+//	vector<int> v2(10);		//0으로 초기화된 10개의 int 방 생성
+//	vector<int> v3(10, 1);	//1로 초기화된 10개의 int 방 생성
+//	vector<int> v4{ 10, 20, 30, 40, 50 };
+//
+//	vector<int> v5(v4);		//다른 vector의 값 가져와서 초기화 가능 
+//	vector<int> v6(v4.begin(), v4.begin() + 3);
+//
+//	for (int i = 0; i < v6.size(); i++)
+//	{
+//		cout << v6[i] << endl;
+//	}
+//}
+
+//동적 메모리를 관리하는 클래스			//인트형 배열
+//template <typename T>
+//class DynamicArray
+//{
+//private :
+//	unsigned int sz;
+//	T* arr;
+//
+//public:
+//	//생성자 : 매개변수를 크기입력받아 동적 메모리에 할당
+//	DynamicArray(int n) : sz(n)
+//	{
+//		arr = new T[sz] {}; //{}초기화 필수
+//	}
+//	//소멸자 : 할당된 메모리를 해제			//클래스에서 메모리 해제 했음
+//	~DynamicArray()
+//	{
+//		delete[] arr;
+//		cout << "동적 메모리 해제 " << endl;
+//	}
+//
+//	unsigned int size() 
+//	{
+//		return sz;
+//	}
+//	T& operator[](const int i) { return arr[i]; }
+//};
+//
+//int main()										
+//{
+//	//메모리 할당
+//	DynamicArray<float>da(5);
+//	da[0] = 10.5;
+//	da[1] = 20.4;
+//	da[2] = 30.3;
+//
+//	for (int i = 0; i < da.size(); i++)
+//	{
+//		cout << da[i] << endl;
+//	}
+//}
+
+//동적 메모리 할당예제
+//int main()
+//{
+//	//동적 메모리 할당
+//	int* ptr = new int[3] {};
+//	ptr[0] = 10;
+//	ptr[1] = 20;
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << ptr[i] << endl;
+//	}
+//	//동적 메모리 해제
+//	delete[] ptr;
+//	ptr = nullptr;
+//}
+//
 //C++언어 배열 구현 - 점수들의 평균을 구하라
 //int main()
 //{
---
+++ ListTest/ListTest.cpp
@@ -0,0 +1,98 @@
+﻿// ListTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+struct Node
+{
+    int data;                   //저장할 데이터
+    Node* next;             //다음에 오는 노드 
+};
+
+class LinkedList
+{
+private :
+    Node* head;           //맨 앞에 있는 노드              
+
+public:
+    //생성자
+    LinkedList() : head(NULL)   //해드는 Null로 기준 
+    {
+    }
+    //소멸자
+    ~LinkedList() 
+    {
+        //생성한 node의 메모리 해제
+        while(!empty())
+        {
+            pop_front();
+        }
+    }
+
+    //매개변수롤 데이터 (value)를 입력받아 리스트 맨 앞에 삽입하기 //노드형태
+    void push_front(int value) 
+    {
+        //데이터(value)를 가진 노드 생성 
+        Node* new_node = new Node{ value, NULL };
+        
+        if (head != NULL)
+        {
+            new_node->next = head;      //현재 head 가 가르키는 자리 
+        }
+
+        head = new_node;
+    }
+
+    //리스트 맨 앞에 있는 데이터 삭제하기
+    void pop_front()
+    {
+        if (head == NULL)           //맨앞에있는 데이터 삭제 
+            return;     
+
+        Node* first = head;
+        head = head->next;      //원래 첫번째에 있던 넥스트에 덮어씌우기 
+        delete first;               //첫번째 메모리 삭제
+    }
+
+    //리스트들 출력 
+    void print_all()
+    {
+        Node* curr = head;
+
+        //curr가 null이 될때까지 반복
+        while (curr != NULL)
+        {
+            //현재 curr 노드의 데이터를 출력
+            cout << curr->data << ", ";
+
+            //curr 노드를 다음 노드로 이동한다
+            curr = curr->next;
+        }
+        cout << endl;
+    }
+    //데이터 유무
+    bool empty()                            //비어있을때 true
+    {
+        return head == NULL;
+    }
+
+};
+
+int main()
+{
+    LinkedList ll;
+    ll.push_front(10);
+    ll.push_front(20);
+    ll.push_front(30);
+    ll.print_all();
+
+    ll.pop_front();
+    ll.print_all();
+
+    ll.push_front(40);
+    ll.print_all();
+}
+
